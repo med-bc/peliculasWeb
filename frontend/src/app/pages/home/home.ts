4e0b5342a5bf4aa3c46dcf9e9f4d28a794b21dd9
ju4n0ff <xlol553@gmail.com>
2026-05-12 14:36:38 -0500

    primera version de trailer

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CatalogoService, Pelicula, Serie } from '../../services/catalogo';
+
+declare var YT: any;
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,13 @@ import { CatalogoService, Pelicula, Serie } from '../../services/catalogo';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   peliculas: Pelicula[] = [];
   series: Serie[] = [];
+  videoPlaying = false;
+  thumbnailError = false;
+  private player: any;
+  private playerReady = false;
 
   constructor(private catalogoService: CatalogoService) {}
 
@@ -26,5 +32,60 @@ export class HomeComponent implements OnInit {
       next: (data) => (this.series = data),
       error: () => (this.series = [])
     });
+
+    this.loadYouTubeAPI();
+  }
+
+  ngOnDestroy() {
+    if (this.player && this.player.destroy) {
+      this.player.destroy();
+    }
+  }
+
+  private loadYouTubeAPI() {
+    if (document.querySelector('script[src*="youtube.com/iframe_api"]')) {
+      this.waitForYouTubeAPI();
+    } else {
+      const script = document.createElement('script');
+      script.src = 'https://www.youtube.com/iframe_api';
+      document.head.appendChild(script);
+      script.onload = () => this.waitForYouTubeAPI();
+    }
+  }
+
+  private waitForYouTubeAPI() {
+    const check = setInterval(() => {
+      if (typeof YT !== 'undefined' && YT.Player) {
+        clearInterval(check);
+        this.initPlayer();
+      }
+    }, 100);
+  }
+
+  private initPlayer() {
+    this.player = new YT.Player('youtube-player', {
+      videoId: 'rQvIR1oL1vE',
+      playerVars: {
+        autoplay: 0,
+        controls: 1,
+        showinfo: 0,
+        modestbranding: 1,
+        rel: 0
+      },
+      events: {
+        onReady: (event: any) => {
+          this.playerReady = true;
+        }
+      }
+    });
+  }
+
+  playVideo(): void {
+    this.videoPlaying = true;
+    setTimeout(() => {
+      if (this.player && this.player.playVideo) {
+        this.player.playVideo();
+      }
+    }, 100);
   }
 }
